32937aa3faabe6f8efaf9d6cfea8eab4490b01c9
Leina Pulgarin <leinatamayo@gmail.com>
2020-03-22 10:49:33 -0500

    Sprint Tres (se modifico un items)

--- Arreglos y Bucles/script/retoSeis.js
+++ Arreglos y Bucles/script/retoSeis.js
@@ -1,18 +1,54 @@
 //guardo en una constante los elementos html que necesito
-const firstFavorite = document.getElementById("firstFavorite");
-const secondFavorite = document.getElementById("secondFavorite");
-const button = document.getElementById("button");
-// const capturado = document.getElementById("firstFavorite").value;
+const viewArray = () => {
+    var arrayInput = new Array();
+    var inputsValues = document.getElementsByClassName("inputText"),
+        namesValues = [].map.call(inputsValues, dataInput => {
+            arrayInput.push(dataInput.value);
+        });
 
-firstFavorite.addEventListener("focus", () => {
-    console.log("Estas haciendo foco en el input");
+    for (const obra of arrayInput) {
+        alert(
+            `¡A mí también me encantó "${obra}"! Tenemos mucho en común, humana.`
+        );
+    }
+    // OTRA FORMA DE RECORER EL ARREGLO CON forEach:
+
+    //     arrayInput.forEach(inputsValuesData => {
+    //         console.log(
+    //             `¡A mí también me encantó "${inputsValuesData}"! Tenemos mucho en común, humana.`
+    //         );
+    //     });
+    // };
+
+    //
+};
+
+///retiro los eventos que trae el boton por defecto
+const boton = document.getElementsByClassName("boton");
+document.addEventListener("click", e => {
+    e.preventDefault();
 });
-secondFavorite.addEventListener("focus", () => {
-    console.log("Estas haciendo foco en el input");
-});
-button.addEventListener("click", () => {
-    event.preventDefault();
-    console.log("Estas precionando el boton enviar");
-    // console.log(capturado);
-});
+
+//BORRADOR
+// var firstFavorite = document.getElementById("firstFavorite");
+
+// const inputUno=()
+// firstFavorite.addEventListener("keyup", e => {
+//     console.log(e.target);
+// });
+// const secondFavorite = document.getElementById("secondFavorite");
+// const button = document.getElementById("button");
+
+// firstFavorite.addEventListener("keyup", e => {
+//     console.log("Estas haciendo foco en el input");
+//     console.log(e.path[0].value);
+// });
+// secondFavorite.addEventListener("focus", () =>
+//     console.log("Estas haciendo foco en el input")
+// );
+// button.addEventListener("click", () => {
+//     event.preventDefault();
+//     console.log("Estas precionando el boton enviar");
+//     console.log(capturado);
+// });
 // continuar
